83129f5e31c5ae1e2d0c2420b48404a185585fb0
Atul <atulmisrajob@gmail.com>
2020-12-29 14:34:15 +0530

    test

---
+++ src/main/java/com/atul/DemoController.java
@@ -0,0 +1,13 @@
+package com.atul;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class DemoController {
+
+	@RequestMapping("/demo")
+	String getDemo() {
+		return"Hi";
+	}
+}
